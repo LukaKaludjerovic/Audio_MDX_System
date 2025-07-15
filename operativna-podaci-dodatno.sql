e2fe1b64d6b4b41616f2a546404291040937fb48
Luka Kaludjerovic <kaludjerovic.l9@icloud.com>
2025-07-16 00:12:06 +0200

    filling cubes done

--- operativna-podaci-dodatno.sql
+++ operativna-podaci-dodatno.sql
@@ -15,8 +15,8 @@ INSERT INTO kategorija (Naziv) VALUES
 ('bas');
 
 INSERT INTO audio (Naziv, Trajanje, IdKor, Datum, Vreme) VALUES
-('Sve po starom', 220, 11, '2025-07-07', '10:00:00'),
-('Dam dam dam', 201, 13, '2024-07-07', '11:00:00');
+('Sve po starom', 220, 11, DATE(NOW()), TIME(NOW())),
+('Dam dam dam', 201, 13, DATE(NOW()), TIME(NOW()));
 
 INSERT INTO audio_kategorija (IdAud, IdKat) VALUES
 (16, 11),
@@ -28,15 +28,15 @@ INSERT INTO paket (MesecnaCena) VALUES
 (2500);
 
 INSERT INTO pretplata (IdKor, IdPak, Datum, Vreme, PlacenaCena) VALUES
-(11, 11, '2025-01-01', '18:00:00', 1500),
-(13, 11, '2025-02-02', '19:00:00', 1450),
-(12, 12, '2024-03-01', '12:00:00', 2380),
-(13, 11, '2024-03-01', '13:00:00', 1465);
+(11, 11, DATE(NOW()), TIME(NOW()), 1500),
+(13, 11, DATE(NOW()), TIME(NOW()), 1450),
+(12, 12, DATE(NOW()), TIME(NOW()), 2380),
+(13, 11, DATE(NOW()), TIME(NOW()), 1465);
 
 INSERT INTO slusanje (IdAud, IdKor, Datum, Vreme, PocetniSekund, TrajanjeUSekundama) VALUES
-(16, 11, '2025-01-01', '18:00:00', 0, 100),
-(16, 13, '2025-02-02', '19:00:00', 8, 158);
+(16, 11, DATE(NOW()), TIME(NOW()), 0, 100),
+(16, 13, DATE(NOW()), TIME(NOW()), 8, 158);
 
 INSERT INTO ocena (IdKor, IdAud, Ocena, Datum, Vreme) VALUES
-(11, 16, 5, '2025-01-01', '18:00:00'),
-(13, 16, 8, '2025-02-02', '19:00:00');
+(11, 16, 5, DATE(NOW()), TIME(NOW())),
+(13, 16, 8, DATE(NOW()), TIME(NOW()));
