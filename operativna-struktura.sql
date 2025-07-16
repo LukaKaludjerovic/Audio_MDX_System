1b6a38b0baa61fa5331d9bd2a16b3afddb730cc3
Luka Kaludjerovic <kaludjerovic.l9@icloud.com>
2025-07-16 22:40:17 +0200

    incremental filling done

--- operativna-struktura.sql
+++ operativna-struktura.sql
@@ -5,6 +5,7 @@ DROP TABLE IF EXISTS `mesto`;
 CREATE TABLE `mesto` (
   `IdMes` int NOT NULL AUTO_INCREMENT,
   `Mesto` varchar(45) DEFAULT NULL,
+  `PoslednjaIzmena` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`IdMes`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -16,6 +17,7 @@ CREATE TABLE `korisnik` (
   `Godiste` int DEFAULT NULL,
   `Pol` varchar(45) DEFAULT NULL,
   `IdMes` int NOT NULL,
+  `PoslednjaIzmena` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`IdKor`),
   KEY `FK_IdMes_idx` (`IdMes`),
   CONSTRAINT `FK_Korisnik_IdMes` FOREIGN KEY (`IdMes`) REFERENCES `mesto` (`IdMes`) ON DELETE RESTRICT ON UPDATE CASCADE
@@ -25,6 +27,7 @@ DROP TABLE IF EXISTS `kategorija`;
 CREATE TABLE `kategorija` (
   `IdKat` int NOT NULL AUTO_INCREMENT,
   `Naziv` varchar(45) DEFAULT NULL,
+  `PoslednjaIzmena` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`IdKat`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -36,6 +39,7 @@ CREATE TABLE `audio` (
   `IdKor` int NOT NULL,
   `Datum` date DEFAULT NULL,
   `Vreme` time DEFAULT NULL,
+  `PoslednjaIzmena` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`IdAud`),
   KEY `FK_IdKor_idx` (`IdKor`),
   CONSTRAINT `FK_Audio_IdKor` FOREIGN KEY (`IdKor`) REFERENCES `korisnik` (`IdKor`) ON DELETE RESTRICT ON UPDATE CASCADE
@@ -45,6 +49,7 @@ DROP TABLE IF EXISTS `audio_kategorija`;
 CREATE TABLE `audio_kategorija` (
   `IdAud` int NOT NULL,
   `IdKat` int NOT NULL,
+  `PoslednjaIzmena` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`IdAud`,`IdKat`),
   KEY `FK_IdKat_idx` (`IdKat`),
   CONSTRAINT `FK_AudioKategorija_IdAud` FOREIGN KEY (`IdAud`) REFERENCES `audio` (`IdAud`) ON DELETE RESTRICT ON UPDATE CASCADE,
@@ -55,6 +60,7 @@ DROP TABLE IF EXISTS `paket`;
 CREATE TABLE `paket` (
   `IdPak` int NOT NULL AUTO_INCREMENT,
   `MesecnaCena` int DEFAULT NULL,
+  `PoslednjaIzmena` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`IdPak`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -66,6 +72,7 @@ CREATE TABLE `pretplata` (
   `Datum` date DEFAULT NULL,
   `Vreme` time DEFAULT NULL,
   `PlacenaCena` int DEFAULT NULL,
+  `PoslednjaIzmena` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`IdPre`),
   KEY `FK_IdKor_idx` (`IdKor`),
   KEY `FK_IdPak_idx` (`IdPak`),
@@ -82,6 +89,7 @@ CREATE TABLE `slusanje` (
   `Vreme` time DEFAULT NULL,
   `PocetniSekund` int DEFAULT NULL,
   `TrajanjeUSekundama` int DEFAULT NULL,
+  `PoslednjaIzmena` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`IdSlu`),
   KEY `FK_Slusanje_IdAud_idx` (`IdAud`),
   KEY `FK_Slusanje_IdKor_idx` (`IdKor`),
@@ -96,6 +104,7 @@ CREATE TABLE `ocena` (
   `Ocena` int DEFAULT NULL,
   `Datum` date DEFAULT NULL,
   `Vreme` time DEFAULT NULL,
+  `PoslednjaIzmena` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`IdKor`,`IdAud`),
   KEY `FK_Ocena_IdAud_idx` (`IdAud`),
   CONSTRAINT `FK_Ocena_IdAud` FOREIGN KEY (`IdAud`) REFERENCES `audio` (`IdAud`) ON DELETE RESTRICT ON UPDATE CASCADE,
